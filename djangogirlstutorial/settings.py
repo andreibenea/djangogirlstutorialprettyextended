f2ec78c342cc5b3bc199e27ed3bee0db68868db2
Andrei Benea <andrei.benea89@gmail.com>
2024-11-29 16:04:30 -0500

    homework - secure your website

--- djangogirlstutorial/settings.py
+++ djangogirlstutorial/settings.py
@@ -123,3 +123,8 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Login redirects
+
+LOGIN_REDIRECT_URL = "/"
